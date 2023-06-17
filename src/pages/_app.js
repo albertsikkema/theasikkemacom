690d59d11695606c1ce08c131d3b61573c5cedd2
Albert Sikkema <github@albertsikkema.com>
2023-06-17 10:30:30 +0200

    Added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
-import '@/styles/globals.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
